6c1b0177ff69660103bf88e6344ae7bc7484bef2
Boban Sugareski <boban@actionvfx.com>
2023-02-24 16:03:08 +0100

    Add basic devise implementation

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
+  devise_for :users, {
+    sessions: 'users/sessions',
+    registrations: 'users/registrations'
+  }
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "products#index"
+
+  get :products, to: "products#index"
+  get 'products/show', to: 'products#show'
 end
